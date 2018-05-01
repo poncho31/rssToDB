d006ce0f814917e66ab711d6ea99967891806a77
greenLine <gil_p54@hotmail.com>
2018-05-01 16:59:56 +0200

    add

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta charset="UTF-8">
 	<meta http-equiv="refresh" content="600; URL=RSS.php">
 	<title>RSS TEST</title>
 	<link rel="stylesheet" href="style.css">
