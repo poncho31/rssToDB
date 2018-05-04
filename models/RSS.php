cc4a7aea525595fbd6e7ca8437365a336ebcc90f
gilles pinchart <gilles.pinchart@gmail.com>
2018-05-04 19:42:51 +0200

    reorganized mysql and mssqlserver files

--- models/RSS.php
+++ models/RSS.php
@@ -161,6 +161,42 @@ $feeds =
 rssToDB($feeds);
 
 
-include '../view/viewRSS.php';
+//SELECT FROM DB - AFFICHAGE DES DONNEES
+try {
+    $db = new PDO("mysql:host=localhost;dbname=rss;charset=utf8","root", "");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $db->exec("set names utf8");
+}
+catch (PDOException $e)
+{
+	die('<span style="color:black">Erreur :  : ' . $e->getMessage()) . '</span>';
+}
+echo  "<p>ARTICLE ISSU DE LA BDD</p>";
+$sqlSELECT = "SELECT * FROM media order by idMedia DESC";
+foreach ($db->query($sqlSELECT) as $row) {
+	?>
+	<table>
+		<tr>
+			<td>ID</td>
+			<td>Media</td>
+			<td>Titre</td>
+			<td>Description</td>
+			<td>Date</td>
+			<td>Lien</td>
+			<td>Categorie</td>
+		</tr>
+		<tr>
+			<td><?php   echo $row['idMedia'] ?></td>
+			<td><?php   echo $row['nom'] ?></td>
+			<td><?php 	echo $row['titre']; ?></td>
+			<td><?php 	echo $row['description']; ?></td>
+			<td><?php 	echo $row['date']; ?></td>
+			<td><?php 	echo $row['lien']; ?></td>
+			<td><?php 	echo $row['categorie']; ?></td>
+		</tr>
+	</table>
+	<?php 
+	
+}
 
 include '../view/footer.php';
--- models/RSStoMsSQLserver.php
+++ models/RSStoMsSQLserver.php
@@ -189,6 +189,43 @@ rssToDB($feeds);
 
 
 
-include '../view/viewRSS.php';
+//SELECT FROM DB - AFFICHAGE DES DONNEES
+try {
+    $db = new PDO("sqlsrv:Server=$serverName;Database=rss","greenline", "test1234=");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::SQLSRV_ATTR_ENCODING, PDO::SQLSRV_ENCODING_UTF8);
+    // $db->exec("set names utf8");
+}
+catch (PDOException $e)
+{
+	die('<span style="color:black">Erreur :  : ' . $e->getMessage()) . '</span>';
+}
+echo  "<p>ARTICLE ISSU DE LA BDD</p>";
+$sqlSELECT = "SELECT * FROM $dbo.media order by idMedia DESC";
+foreach ($db->query($sqlSELECT) as $row) {
+	?>
+	<table>
+		<tr>
+			<td>ID</td>
+			<td>Media</td>
+			<td>Titre</td>
+			<td>Description</td>
+			<td>Date</td>
+			<td>Lien</td>
+			<td>Categorie</td>
+		</tr>
+		<tr>
+			<td><?php   echo $row['idMedia'] ?></td>
+			<td><?php   echo $row['nom'] ?></td>
+			<td><?php 	echo $row['titre']; ?></td>
+			<td><?php 	echo $row['description']; ?></td>
+			<td><?php 	echo $row['date']; ?></td>
+			<td><?php 	echo $row['lien']; ?></td>
+			<td><?php 	echo $row['categorie']; ?></td>
+		</tr>
+	</table>
+	<?php 
+	
+}
 
 include '../view/footer.php';
