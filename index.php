a0e3cfc25ac8a714adec61b471712d297f6b25ac
greenLine <gil_p54@hotmail.com>
2018-05-06 04:08:21 +0200

    Add interface to project

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>DB</title>
+	<link rel="stylesheet" href="design/css/style.css">
 </head>
-<body>
-
-	<a href="models/RSS.php"><button>MySQL</button></a>
-	<a href="models/RSStoMsSQLserver.php"><button>MS SQL server</button></a>
-	<a href="models/MysSQLsaveDB.php"><button>Save MySQL Database</button></a>
+<body class="interface">
+	<div class="grid">
+		<div><a href="models/RSS.php"><button>MySQL</button></a></div>
+		<div><a href="models/RSStoMsSQLserver.php"><button>MS SQL server</button></a></div>
+		<div><a href="models/MysSQLsaveDB.php"><button>Save MySQL Database</button></a></div>
+	</div>
 
 </body>
 </html>
