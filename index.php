5ee3d9e65b7bf0b99062d233b8c7aea80e5e3b7d
greenLine <gil_p54@hotmail.com>
2018-05-06 04:51:50 +0200

    Interface css

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
 	<link rel="stylesheet" href="design/css/style.css">
 </head>
 <body class="interface">
-	<div class="grid">
-		<div><a href="models/RSS.php"><button>MySQL</button></a></div>
-		<div><a href="models/RSStoMsSQLserver.php"><button>MS SQL server</button></a></div>
-		<div><a href="models/MysSQLsaveDB.php"><button>Save MySQL Database</button></a></div>
+	<div class="transparent">
+		<h1>What's your politician ? </h1>
+		<div class="grid">
+			<div><a href="models/RSS.php"><button>MySQL</button></a></div>
+			<div><a href="models/RSStoMsSQLserver.php"><button>MS SQL server</button></a></div>
+			<div><a href="models/MysSQLsaveDB.php"><button>Save MySQL Database</button></a></div>
+		</div>
 	</div>
 
 </body>
