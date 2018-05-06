36f3c4a947d7c54599c6218a86fe53b0b731ee70
greenLine <gil_p54@hotmail.com>
2018-05-06 14:14:22 +0200

    Automated rss to DB => 2018-05-06 14:14:22

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 </head>
 <body class="interface">
 	<div class="transparent">
-		<h1>What's your politician ? </h1>
+		<h1>Application's Name</h1>
 		<div class="grid">
 			<div><a href="models/RSS.php"><button>MySQL</button></a></div>
 			<div><a href="models/RSStoMsSQLserver.php"><button>MS SQL server</button></a></div>
-			<div><a href="models/MysSQLsaveDB.php"><button>Save MySQL Database</button></a></div>
+			<div><a href="models/MySQLsaveDB.php"><button>Save MySQL Database</button></a></div>
 		</div>
 	</div>
 
---
+++ rssToDbAutomatized.bat
@@ -0,0 +1,5 @@
+@echo off
+php models\automatedMysqlToDb.php
+php models\MySQLsaveDB.php
+rem php models\RSStoMsSQLserver.php
+pause
