cb0e253a2b4bf8e58bff0b39e5a4376e2cea36af
greenline <gilles.pinchart@gmail.com>
2018-05-21 15:23:13 +0200

    Automated rss to DB => 2018-05-21 15:23:13

--- models/MySQLsaveDB.php
+++ models/MySQLsaveDB.php
@@ -5,6 +5,11 @@ try {
 	$db = new mysqli('localhost', 'root', '', 'rss');
 	$dump = new MySQLDump($db);
 	$dump->save('../data/'.date("y.m.d").'-SQLsave.sql.gz');
+	echo exec(escapeshellcmd('git add .
+git commit -m "Automated rss to DB => `date +"%Y-%m-%d %H:%M:%S"`"
+git push origin automatedRSS
+'), $output);
+	print_r($output);
 	echo "Saved without errors : " . date("y.m.d");
 
 } catch (Exception $e) {
