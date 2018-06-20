b11937cee86edec5d30d950985f9762800c87e93
greenline <gilles.pinchart@gmail.com>
2018-06-20 16:19:25 +0200

    add

--- view/header.php
+++ view/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta http-equiv="refresh" content="1000; URL=RSS.php">
+	<meta http-equiv="refresh" content="1000; URL=?section=mysql">
 	<title>RSS TEST</title>
 	<link rel="stylesheet" href="../design/css/style.css">
 </head>
