f74bd134d65c1ab885f7a160b87528c338b6b9b3
greenline <gilles.pinchart@gmail.com>
2018-06-20 16:55:47 +0200

    add

--- models/MySQLsaveDB.php
+++ models/MySQLsaveDB.php
@@ -5,8 +5,14 @@ try {
 	$db = new mysqli('localhost', 'root', '', 'rss');
 	$dump = new MySQLDump($db);
 	$dump->save('data/'.date("y.m.d").'-SQLsave.sql.gz');
-	$pathContents = file_get_contents('GitPushRssToDB.sh');
-	echo exec($pathContents);
+	$pathContents = file_get_contents('./GitPushRssToDB.sh');
+	$output = shell_exec("git add .");
+	$output .= shell_exec('git commit -m "Automated rss to DB "');
+	echo $output;
+
+	if ($output) {
+		echo "GIT PUSH DONE :" . $pathContents . "<br>";
+	}
 	echo "Saved without errors : " . date("y.m.d");
 
 } catch (Exception $e) {
