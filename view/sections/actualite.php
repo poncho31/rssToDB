4b42f920b1acede0592ab4546307bc61ee50ae30
gilles pinchart <gilles.pinchart@gmail.com>
2018-07-27 22:06:10 +0200

    add

--- view/sections/actualite.php
+++ view/sections/actualite.php
@@ -4,6 +4,23 @@ use Poncho\Database\Database;
 require 'vendor/autoload.php';
 $db = new Database();
 
+
+  $sql = 'SELECT count(*) as countMedia, nom FROM media GROUP BY nom';
+  $stmt = $db->getQuery($sql);
+  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  $arr = [];
+  foreach ($rows as $row) {
+    $arr[$row['nom']] = $row['countMedia'];
+    echo $row['nom'] ."<br>";
+  }
+  $jsonData = json_encode($arr);
+  echo "<br>".$jsonData;
+  // $fp = fopen('data/nombreArticleParMedia.json', 'w');
+  // fwrite($fp, $jsonData);
+
+
+
 echo  "<p>ARTICLE ISSU DE LA BDD</p>";
 $sqlSELECT = "SELECT * FROM media order by idMedia DESC LIMIT 0, 20";
 
